189fc32d6435562ca130fac6ac4afa95f10cea70
asweingarten <asweingarten@gmail.com>
2019-12-15 16:24:14 -0500

    Connected p5 and codemirror

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import CodeMirror from 'react-codemirror';
 import logo from './logo.svg';
 import './App.css';
-import P5 from './P5'
 
-require('codemirror/lib/codemirror.css');
-require('codemirror/mode/javascript/javascript');
+import IO from './IO/IO';
 
 function App() {
-  const Sketch = P5();
-  const codeMirrorOptions = {
-    lineNumbers: true,
-    mode: 'javascript'
-  };
+
   return (
     <div className="App">
-
       <main style={{display: "flex", flexFlow: "row nowrap"}}>
-        <div style={{width: "300px"}}>
-          <CodeMirror
-            value={"hi"}
-            onChange={() => console.log("code change")}
-            options={codeMirrorOptions}
-          />
-        </div>
-        <div style={{width: "300px"}}>
-          <Sketch
-          />
-        </div>
+        <IO />
       </main>
     </div>
   );
